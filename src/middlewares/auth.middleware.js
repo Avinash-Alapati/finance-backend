a4f6eb4fff8f3f386a415af3af40c704e590825c
Avinash-Alapati <avinashalapati11@gmail.com>
2026-04-02 16:59:21 +0530

    removed hardcoded JWT secret

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ export const protect = async (req, res, next) => {
   token = token.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, "secret");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.id);
     next();
   } catch {
